b0f886e9945c1aae6e0d1098a021d6245c3b42ad
yussuf <yussuf.ibrahim@student.moringaschool.com>
2024-11-19 23:08:37 +0300

    class book

--- app/models.py
+++ app/models.py
@@ -8,3 +8,10 @@ class User(db.Model):
     avatar = db.Column(db.String(255))
     role = db.Column(db.String(20), nullable=False)
 
+class Book(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    cover_image = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+
