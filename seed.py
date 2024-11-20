09bd7f5e8bb3e109b1f20f9deff871014418b832
fiona <fiona.mburu@student.moringaschool.com>
2024-11-20 08:24:38 +0300

    Load environment variables from .env file

--- seed.py
+++ seed.py
@@ -123,7 +123,7 @@ def seed_data():
         # Add books to clubs
         club1.books = [book1]
         club2.books = [book2]
-        club3.books = [book3]
+        club3.books = [book3] 
 
         # Commit the changes
         db.session.commit()
